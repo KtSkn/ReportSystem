5f509166db74418c683500b9d8571a4987ea51c3
kkkkkkssssss <yuzuoAAAA@gmail.com>
2023-08-19 23:50:51 +0900

    SQL追加

---
+++ demo/sql/chohyo_insert.sql
@@ -0,0 +1,27 @@
+/*
+-- Query: SELECT * FROM spring_test.chohyo
+LIMIT 0, 1000
+
+-- Date: 2023-08-19 23:46
+*/
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (1,'0001','aaaa','1111',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (2,'0001','aaaa','1111',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (3,'0001','aaaa','1111',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (4,'0001','aaaa','1111',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (5,'0001','aaaa','1111',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (6,'0002','aaaa','1111',2,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (7,'0002','aaaa','1111',2,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (8,'0002','bbbb','1111',2,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (9,'0002','bbbb','2222',3,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (10,'0002','bbbb','2222',3,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (11,'0002','bbbb','2222',3,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (12,'0002','bbbb','2222',3,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (13,'0002','bbbb','2222',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (14,'0003','bbbb','2222',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (15,'0003','bbbb','2222',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (16,'0003','bbbb','2222',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (17,'0003','bbbb','2222',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (18,'0003','bbbb','2222',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (19,'0003','bbbb','2222',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (20,'0003','bbbb','2222',1,2,3);
+INSERT INTO `` (`id`,`header1`,`header2`,`header3`,`meisai1`,`meisai2`,`meisai3`) VALUES (21,'0003','bbbb','2222',1,2,3);
---
+++ demo/sql/fundmast_insert.sql
@@ -0,0 +1,36 @@
+/*
+-- Query: SELECT * FROM spring_test.fundmst
+LIMIT 0, 1000
+
+-- Date: 2023-08-19 23:48
+*/
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (1,'aaaaaaaaaaaaaaaaaaaaaaaa','bbb',1.00000,'header2','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (2,'ccc','ddd',2.00000,'header2','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (3,'','fff',3.00000,'header2','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (4,'aaa','bbb',1.00000,'header2','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (5,'aaa','bbb',1.00000,'header2','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (6,'aaa','bbb',1.00000,'header2','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (7,'aaa','bbb',1.00000,'header2','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (8,'aaa','bbb',1.00000,'header2','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (9,'aaa','bbb',1.00000,'header2','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (10,'aaa','bbb',1.00000,'header2','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (11,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (12,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (13,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (14,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (15,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (16,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (17,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (18,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (19,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (20,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (21,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (22,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (23,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (24,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (25,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (26,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (27,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (28,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (29,'aaa','bbb',1.00000,'header12','header2');
+INSERT INTO `` (`id`,`fund1`,`fund2`,`hakabu`,`header1`,`header2`) VALUES (30,'aaa','bbb',1.00000,'header14','header2');
